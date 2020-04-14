4c65a9a2b1fe1fe3548b67f88f30be88ef2c72e6
DenisKantic <denis.kantic18@gmail.com>
2020-04-14 20:03:06 +0200

    added hidden and visible properties in javascript

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+document.getElementById('result-container').style.visibility = 'hidden';
+
+
 document.getElementById('inputValue').addEventListener('input', function(e){
 
+    
+    document.getElementById('result-container').style.visibility = 'visible';
     let userInput = e.target.value;
 
     document.getElementById('gValue').innerHTML = userInput*1000;
